36e8eaed3f905f9e2bef587a476763c0daeac9a8
shahiankur <shahiankur2@gmail.com>
2023-09-27 17:50:40 +0530

    adding crash/properties/scene delegate call to VC

--- pilot-ci-cd/SceneDelegate.swift
+++ pilot-ci-cd/SceneDelegate.swift
@@ -11,12 +11,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
+        // Create a new UIWindow using the window scene
+        let window = UIWindow(windowScene: windowScene)
+        
+        // Create and set the root view controller
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        window.rootViewController = viewController
+        
+        // Make the window visible
+        window.makeKeyAndVisible()
+        
+        // Assign the window to the scene
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- pilot-ci-cd/ViewController.swift
+++ pilot-ci-cd/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthlyInvestmentsTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var retirementAgeTextField: UITextField!
+    @IBOutlet weak var interestRateTextField: UITextField!
+    @IBOutlet weak var savingsTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if Crashes.hasCrashedInLastSession {
+            let alert = UIAlertController(title: "Oops", message: "An error might have occured", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+            self.present(alert, animated: true)
+        }
+        Analytics.trackEvent("navigated_to_calculate_retirement_screen")
+    }
 
 
+    @IBAction func calculateButton_TouchUpInside(_ sender: Any) {
+//        Crashes.generateTestCrash()
+        let currentAge = ageTextField.text ?? ""
+        let retirementAge = retirementAgeTextField.text ?? ""
+        
+        let properties = ["currentAge": currentAge, "retirementAge": retirementAge]
+        
+        Analytics.trackEvent("calculate_retirement_button_tapped", withProperties: properties)
+    }
+    
 }
 
