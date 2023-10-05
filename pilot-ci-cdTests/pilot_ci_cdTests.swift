7064e2e92e8a51ffc2f3530b044dcae241c69876
shahiankur <shahiankur2@gmail.com>
2023-10-05 14:24:28 +0530

    UI test written - failed

--- pilot-ci-cdTests/pilot_ci_cdTests.swift
+++ pilot-ci-cdTests/pilot_ci_cdTests.swift
@@ -16,7 +16,7 @@ final class pilot_ci_cdTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         storyboard = UIStoryboard(name: "Main", bundle: nil)
-        sut = storyboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        sut = storyboard.instantiateViewController(withIdentifier: "ViewController") as? ViewController
         sut.loadViewIfNeeded()
     }
 
