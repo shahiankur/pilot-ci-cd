e5c279ddaa1cd4b1156ca2bd90217a61fbb7ca4b
shahiankur <shahiankur2@gmail.com>
2023-10-05 18:27:15 +0530

    disabled failed tests

--- pilot-ci-cdUITests/pilot_ci_cdUITests.swift
+++ pilot-ci-cdUITests/pilot_ci_cdUITests.swift
@@ -73,27 +73,27 @@ final class pilot_ci_cdUITests: XCTestCase {
         XCTAssertEqual(resultLabel.label, "If you save $100.0 every month for 33 years, and invest that money plus your current investment of $200.0 at a 12.0% anual interest rate, you will have $6.185398792968953e+21 by the time you are 65")
     }
 
-    func testRetirementViewController_whenValuesProvided_resultLabelDisplaysCorrectValues_Failed() throws {
-        // UI tests must launch the application that they test.
-        monthlyInvestments.tap()
-        monthlyInvestments.typeText("100")
-        
-        currentAge.tap()
-        currentAge.typeText("32")
-        
-        retirementAge.tap()
-        retirementAge.typeText("65")
-        
-        rateOfInterest.tap()
-        rateOfInterest.typeText("12")
-        
-        currentSavings.tap()
-        currentSavings.typeText("200")
-        
-        calculateRetirmentAmountButton.tap()
-        
-        XCTAssertEqual(resultLabel.label, "If you save $100.0 every month for 33 years, and invest that money plus your current investment of $200.0 at a 12.0% anual interest rate, you will have $6.185398792968953e+21 by the time you are 65.")
-    }
+//    func testRetirementViewController_whenValuesProvided_resultLabelDisplaysCorrectValues_Failed() throws {
+//        // UI tests must launch the application that they test.
+//        monthlyInvestments.tap()
+//        monthlyInvestments.typeText("100")
+//        
+//        currentAge.tap()
+//        currentAge.typeText("32")
+//        
+//        retirementAge.tap()
+//        retirementAge.typeText("65")
+//        
+//        rateOfInterest.tap()
+//        rateOfInterest.typeText("12")
+//        
+//        currentSavings.tap()
+//        currentSavings.typeText("200")
+//        
+//        calculateRetirmentAmountButton.tap()
+//        
+//        XCTAssertEqual(resultLabel.label, "If you save $100.0 every month for 33 years, and invest that money plus your current investment of $200.0 at a 12.0% anual interest rate, you will have $6.185398792968953e+21 by the time you are 65.")
+//    }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
