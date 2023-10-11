12b957e660a04b25c7edd8ff30c54d7e9c331ebd
shahiankur <shahiankur2@gmail.com>
2023-10-11 10:56:10 +0530

    testing merge request

--- pilot-ci-cdTests/pilot_ci_cdTests.swift
+++ pilot-ci-cdTests/pilot_ci_cdTests.swift
@@ -40,19 +40,19 @@ final class pilot_ci_cdTests: XCTestCase {
         XCTAssertEqual(currentSavings.text, "")
     }
     
-//    func testViewController_whenCreated_hasRequiredFieldsEmpty_Failed() throws {
-//        let mothlyInvestments = try XCTUnwrap(sut.monthlyInvestmentsTextField)
-//        let currentAge = try XCTUnwrap(sut.ageTextField)
-//        let retirementAge = try XCTUnwrap(sut.retirementAgeTextField)
-//        let rateOfInterest = try XCTUnwrap(sut.interestRateTextField)
-//        let currentSavings = try XCTUnwrap(sut.savingsTextField)
-//        
-//        XCTAssertEqual(mothlyInvestments.text, "mothlyInvestments")
-//        XCTAssertEqual(currentAge.text, "currentAge")
-//        XCTAssertEqual(retirementAge.text, "retirementAge")
-//        XCTAssertEqual(rateOfInterest.text, "rateOfInterest")
-//        XCTAssertEqual(currentSavings.text, "currentSavings")
-//    }
+    func testViewController_whenCreated_hasRequiredFieldsEmpty_Failed() throws {
+        let mothlyInvestments = try XCTUnwrap(sut.monthlyInvestmentsTextField)
+        let currentAge = try XCTUnwrap(sut.ageTextField)
+        let retirementAge = try XCTUnwrap(sut.retirementAgeTextField)
+        let rateOfInterest = try XCTUnwrap(sut.interestRateTextField)
+        let currentSavings = try XCTUnwrap(sut.savingsTextField)
+        
+        XCTAssertEqual(mothlyInvestments.text, "mothlyInvestments")
+        XCTAssertEqual(currentAge.text, "currentAge")
+        XCTAssertEqual(retirementAge.text, "retirementAge")
+        XCTAssertEqual(rateOfInterest.text, "rateOfInterest")
+        XCTAssertEqual(currentSavings.text, "currentSavings")
+    }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
